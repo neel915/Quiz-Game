38a603db641a484a42ddb751835b4a6082edf502
neel915 <neelab.sadat2020@gmail.com>
2024-10-28 16:30:04 +0100

    on a fait la squelette html/css.

---
+++ questions.js
@@ -0,0 +1,35 @@
+export const cultureGeneral = {
+  questions: [
+    {
+      text: "Qui est le président français actuel?",
+      options: [
+        "François Mitterrand",
+        "Jacques Chirac",
+        "Nicolas Sarkozy",
+        "Emmanuel Macron",
+      ],
+      correct_answer: "Emmanuel Macron",
+    },
+    {
+      text: "Question 2 ?",
+      options: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4"],
+      correct_answer: "Réponse 2",
+    },
+    // Autres questions...
+    {
+      text: "Question 3 ?",
+      options: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4"],
+      correct_answer: "Réponse 3",
+    },
+    {
+      text: "Question 4 ?",
+      options: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4"],
+      correct_answer: "Réponse 4",
+    },
+    {
+      text: "Question 5 ?",
+      options: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4"],
+      correct_answer: "Réponse 1",
+    },
+  ],
+};
